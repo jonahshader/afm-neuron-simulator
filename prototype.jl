77cdc6d65780c56751678c719f7fb9b7744ccb99
Jonah Shader <jonah.shader@gmail.com>
2022-05-24 23:43:17 -0400

    todo: finish prototype

--- prototype.jl
+++ prototype.jl
@@ -1,5 +1,13 @@
 include("Neurons.jl")
 
+mutable struct BufMatrix
+    m::Matrix{Float64}
+    in::Vector{Float64}
+    out::Vector{Float64}
+end
+
+BufMatrix(input_length::Int, output_length::Int) = BufMatrix(zeros(Float64, output_length, input_length), zeros(Float64, input_length), zeros(Float64, output_length))
+
 mutable struct Component
     inputs::Int
     outputs::Int
@@ -13,6 +21,9 @@ mutable struct Component
     non_output_weights::Matrix{Float64}
     non_output_weights_input::Vector{Float64}
     non_output_weights_output::Vector{Float64}
+
+    input::Vector{Float64}
+    output::Vector{Float64}
 end
 
 function build_weights!(c::Component)
@@ -31,8 +42,13 @@ end
 
 function compute_component_output(comp::Component)
 
-    for c in comp
-        c.compute_component_output(c)
+    output_index = 1
+    for c in comp.components
+        compute_component_output(c)
+        next_output_index = output_index + c.outputs
+        neuron_view = view(comp.output_weights_input, output_index:(next_output_index-1))
+        neuron_view .= 
+        output_index = next_output_index
     end
 
     neuron_view = view(comp.output_weights_input, 1:length(comp.neurons))
