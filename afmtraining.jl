2ee8dbcf6a277f542c8bb144f44e4dadab4044c5
jonahshader <jonah.shader@gmail.com>
2022-06-20 00:46:36 -0400

    started working on training example

--- afmtraining.jl
+++ afmtraining.jl
@@ -2,14 +2,13 @@ include("afmcomponent.jl")
 include("afmdiffeq.jl")
 
 function set_nonzeros_trainable!(comp::Component)
-
-    comp.weights_trainable_mask.matrix = comp.weights.matrix .!= 0
+    set_raw!(weights_trainable_mask(comp), raw(weights(comp)) .!= 0)
 end
 
-function loss!(parts::AFMModelParts, input::Vector{Vector{T}}, target_output::Vector{Vector{T}}) where {T<:AbstractFloat}
+function loss!(parts::AFMModelParts, input::Vector{Vector{T}}, target_output::Vector{Vector{T}}; args...) where {T<:AbstractFloat}
     total_loss = 0.0
     for io_pair in zip(input, target_output)
-        total_loss += loss!(parts, io_pair[1], io_pair[2])
+        total_loss += loss!(parts, io_pair[1], io_pair[2], args...)
     end
     total_loss / length(input)
 end
---
+++ training_example.jl
@@ -0,0 +1,61 @@
+include("afmcomponent.jl")
+include("graph/afmgraph.jl")
+include("afmdiffeq.jl")
+include("afmtraining.jl")
+
+using DifferentialEquations
+using Plots
+
+# full_adder = Component(["a", "b", "c-in"], ["sum", "c-out"])
+full_adder = Component(["a", "b", "c-in"], ["sum", "c-out"])
+xor = Component(2, 1)
+add_component!(full_adder, xor, "xor1")
+add_component!(full_adder, xor, "xor2")
+and = Component(2, 1)
+add_component!(full_adder, and, "and1")
+add_component!(full_adder, and, "and2")
+or = Component(2, 1)
+add_component!(full_adder, or, "or1")
+
+scale = 0.4
+set_weight!(full_adder, "a", ("xor1", 1), scale)
+set_weight!(full_adder, "a", ("and2", 2), scale)
+set_weight!(full_adder, "b", ("xor1", 2), scale)
+set_weight!(full_adder, "b", ("and2", 2), scale)
+set_weight!(full_adder, "c-in", ("xor2", 2), scale)
+set_weight!(full_adder, "c-in", ("and1", 1), scale)
+set_weight!(full_adder, ("xor1", 1), ("xor2", 1), scale)
+set_weight!(full_adder, ("xor1", 1), ("and1", 2), scale)
+set_weight!(full_adder, ("xor2", 1), "sum", scale)
+set_weight!(full_adder, ("and1", 1), ("or1", 1), scale)
+set_weight!(full_adder, ("and2", 1), ("or1", 2), scale)
+set_weight!(full_adder, ("or1", 1), "c-out", scale)
+
+add_neurons!(and, 1)
+set_weight!(and, 1, (1,), scale * .5)
+set_weight!(and, 2, (1,), scale * .5)
+set_weight!(and, (1,), 1, scale)
+
+add_neurons!(or, 1)
+set_weight!(or, 1, (1,), scale)
+set_weight!(or, 2, (1,), scale)
+set_weight!(or, (1,), 1, scale * .5)
+
+add_component!(xor, and, "and1")
+add_component!(xor, or, "or1")
+# add_neurons!()
+set_weight!(xor, 1, ("and1", 1), 1.0)
+set_weight!(xor, 2, ("and1", 2), 1.0)
+set_weight!(xor, 1, ("or1", 1), 1.0)
+set_weight!(xor, 2, ("or1", 2), 1.0)
+set_weight!(xor, ("and1", 1), 1, -1.0)
+set_weight!(xor, ("or1", 1), 1, 1.0)
+
+set_nonzeros_trainable!(full_adder)
+set_nonzeros_trainable!(xor)
+set_nonzeros_trainable!(and)
+set_nonzeros_trainable!(or)
+
+# TODO: build weights view (might have implemented this already)
+# yeah, parameters_view from afmdiffeq.jl
+# might need to re-implement though
